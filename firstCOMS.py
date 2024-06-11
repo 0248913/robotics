0743c7e2696f86ab315b2d06cebfb8f8ea9412da
0248913 <142315920+0248913@users.noreply.github.com>
2024-06-11 14:13:23 +1000

    Update firstCOMS.py

--- firstCOMS.py
+++ firstCOMS.py
@@ -3,11 +3,11 @@ import time
 
 
 if __name__ == '__main__':
-   ser = serial.Serial('/dev/ttyAMC0',9600, timeout = 1)
-   ser.reset_input_buffer()
+    ser = serial.Serial('/dev/ttyAMC0',9600, timeout = 1)
+    ser.reset_input_buffer()
 
-   while True:
-       ser.write(b"F/n")
-       line = se.readline().decode('utf-8').rstrip()
-       print(line)
-       time.sleep(1)
+    while True:
+        ser.write(b"F/n")
+        line = se.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(1)
