d6a04705489f38929f5a09ae302ad44a6b6a7e31
0248913 <142315920+0248913@users.noreply.github.com>
2024-06-11 17:09:09 +1000

    Create photoBEV.py

---
+++ photoBEV.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+def apply_birdseye_view(image_path):
+   
+    frame = cv2.imread(image_path)
+    if frame is None:
+        print("Error: Could not load image.")
+        return
+
+    output_width = 320
+    output_height = 240
+
+    
+    pts1 = np.float32([
+        [0, 1080],     # Bottom-left
+        [1920, 1080],  # Bottom-right
+        [1632, 648],   # Top-right
+        [288, 648]     # Top-left
+    ])
+
+   
+    pts2 = np.float32([
+        [0, output_height],          
+        [output_width, output_height], 
+        [output_width, 0],            
+        [0, 0]                        
+    ])
+
+  
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+    birdseye_view = cv2.warpPerspective(frame, M, (output_width, output_height))
+
+   
+    cv2.imshow('Original Frame', frame)
+    cv2.imshow('Birds Eye View', birdseye_view)
+
+   
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    image_path = 'captured_image.jpg'  
+    apply_birdseye_view(image_path)
+
