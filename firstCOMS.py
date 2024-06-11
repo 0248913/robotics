1fceba48471fc626e3a9834a1f513142dcef8563
0248913 <142315920+0248913@users.noreply.github.com>
2024-06-11 14:30:28 +1000

    Update firstCOMS.py

--- firstCOMS.py
+++ firstCOMS.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     ser.reset_input_buffer()
 
     while True:
-        ser.write(b"F/n")
+        ser.write(b"F\n")
         line = se.readline().decode('utf-8').rstrip()
         print(line)
         time.sleep(1)
